3f4a0401fc8c584db7f897268d840de47841fac5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-01 20:12:18 +0530

    configured prettier formatter

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,18 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
 
 const Navbar: React.FC = () => {
-    return (
-        <Flex bg='white' height='44px' padding='6px 12px'>
-            <Flex>
-
-            </Flex>
-        </Flex>
-    )
-}
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: "none", mid: "unset" }}
+        />
+      </Flex>
+    </Flex>
+  );
+};
 export default Navbar;
