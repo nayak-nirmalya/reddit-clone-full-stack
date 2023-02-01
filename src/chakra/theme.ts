4b5c5950ca8daa8dc7e8ffadbb9f335a0f955c56
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-01 12:59:06 +0530

    tweaked theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,3 +1,6 @@
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
 import { extendTheme } from "@chakra-ui/react"
 
 export const theme = extendTheme({
@@ -8,5 +11,15 @@ export const theme = extendTheme({
   },
   fonts: {
     body: "Open Sans, sans-serif"
+  },
+  styles: {
+    global: () => ({
+        body: {
+            bg: 'gray.200'
+        }
+    })
+  },
+  components: {
+    // Button
   }
 })
