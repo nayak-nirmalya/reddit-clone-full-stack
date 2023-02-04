d387c940ab9d4e32c232c6f1972e56bb27487f2c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-04 19:47:57 +0530

    initialized and configured firebase

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +20,11 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase for SSR
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 const analytics = getAnalytics(app);
+
+export { app, firestore, auth, storage, analytics };
