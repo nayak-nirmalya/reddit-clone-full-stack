0620dd2c29d991dd5388dd89aa4d0d5be4006b28
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-05 20:27:46 +0530

    added styling to menu

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,7 +1,21 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  Button,
+  MenuList,
+  MenuItem,
+  Icon,
+  Flex,
+  MenuDivider
+} from "@chakra-ui/react";
 import { User } from "firebase/auth";
 import React from "react";
+import { FaRedditSquare } from "react-icons/fa";
+import { VscAccount } from "react-icons/vsc";
+import { IoSparkles } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineLogin } from "react-icons/md";
 
 type UserMenuProps = {
   user?: User | null;
@@ -10,15 +24,52 @@ type UserMenuProps = {
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-        Actions
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+      >
+        {user ? (
+          <Flex align="center">
+            <Flex align="center">
+              <>
+                <Icon
+                  fontSize={24}
+                  mr={1}
+                  color="gray.300"
+                  as={FaRedditSquare}
+                />
+              </>
+              <ChevronDownIcon />
+            </Flex>
+          </Flex>
+        ) : (
+          <Icon as={VscAccount} fontSize={24} color="gray.400" mr={1} />
+        )}
       </MenuButton>
       <MenuList>
-        <MenuItem>Download</MenuItem>
-        <MenuItem>Create a Copy</MenuItem>
-        <MenuItem>Mark as Draft</MenuItem>
-        <MenuItem>Delete</MenuItem>
-        <MenuItem>Attend a Workshop</MenuItem>
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{ bg: "blue.400", color: "white" }}
+        >
+          <Flex align="center">
+            <Icon as={CgProfile} fontSize={20} mr={2} />
+            Profile
+          </Flex>
+        </MenuItem>
+        <MenuDivider />
+        <MenuItem
+          fontSize="10pt"
+          fontWeight={700}
+          _hover={{ bg: "blue.400", color: "white" }}
+        >
+          <Flex align="center">
+            <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
+            Log Out
+          </Flex>
+        </MenuItem>
       </MenuList>
     </Menu>
   );
