eb736e78779a144391d58d0cb630038c9ea9497c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-08 13:36:35 +0530

    safe-json-stringify added, new atom for type def

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,8 +1,34 @@
+import { Community } from "@/atoms/communitiesAtom";
+import { firestore } from "@/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
 
-type CommunityPageProps = {};
-
-const CommunityPage: React.FC<CommunityPageProps> = () => {
-  return <div>CommunityPage</div>;
+type CommunityPageProps = {
+  communityData: Community;
 };
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  return <div>{communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: communityDoc.data()
+      }
+    };
+  } catch (error) {
+    console.error("getServerSidPropsError", error);
+  }
+}
+
 export default CommunityPage;
