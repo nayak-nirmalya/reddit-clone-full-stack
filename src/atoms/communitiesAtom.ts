ef0ce0b1fac95971ff9b38b2d35b94d3d44b47b9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-08 19:13:30 +0530

    updated communityAtom with recoil

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -1,4 +1,5 @@
 import { Timestamp } from "firebase/firestore";
+import { atom } from "recoil";
 
 export interface Community {
   id: string;
@@ -8,3 +9,23 @@ export interface Community {
   createdAt?: Timestamp;
   imageURL?: string;
 }
+
+interface CommunitySnippet {
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
+}
+
+interface CommunityState {
+  mySnippet: CommunitySnippet[];
+  // visitedCommunity
+}
+
+const defaultCommunityState: CommunityState = {
+  mySnippet: []
+};
+
+export const communityState = atom<CommunityState>({
+  key: "communityState",
+  default: defaultCommunityState
+});
