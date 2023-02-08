39876bc386b0c9fdde52af3f6bb8e8a1f674a591
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-08 13:26:33 +0530

    created clien side route for CommunityPage

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type CommunityPageProps = {};
+
+const CommunityPage: React.FC<CommunityPageProps> = () => {
+  return <div>CommunityPage</div>;
+};
+export default CommunityPage;
