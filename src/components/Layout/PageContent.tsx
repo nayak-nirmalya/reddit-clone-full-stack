851b4a9bf566d7ddfdb4e060ee43031b9ab273fb
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-09 13:23:58 +0530

    added newPostForm componet

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -9,19 +9,13 @@ const PageContent: React.FC<PageContentProps> = ({
   children
 }: PageContentProps) => {
   return (
-    <Flex justify="center" p="16px 0px" border="1px solid red">
-      <Flex
-        width="95%"
-        justify="center"
-        maxWidth="860px"
-        border="1px solid green"
-      >
+    <Flex justify="center" p="16px 0px">
+      <Flex width="95%" justify="center" maxWidth="860px">
         {/* LHS */}
         <Flex
           direction="column"
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
-          border="1px solid blue"
         >
           {children && children[0 as keyof typeof children]}
         </Flex>
@@ -31,7 +25,6 @@ const PageContent: React.FC<PageContentProps> = ({
           direction="column"
           display={{ base: "none", md: "flex" }}
           flexGrow={1}
-          border="1px solid orange"
         >
           {children && children[1 as keyof typeof children]}
         </Flex>
