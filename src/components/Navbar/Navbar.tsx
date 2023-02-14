ca0bf9a3fbf91767d4548997b197ed6d160dd364
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-14 21:23:44 +0530

    show current community name/icon instead of home

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
+import { defaultMenuItem } from "@/atoms/directoryMenuAtom";
 import { auth } from "@/firebase/clientApp";
+import useDirectory from "@/hooks/useDirectory";
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,6 +10,8 @@ import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
+
   return (
     <Flex
       bg="white"
@@ -23,10 +27,9 @@ const Navbar: React.FC = () => {
           base: "40px",
           md: "auto"
         }}
-        mr={{
-          base: 0,
-          md: 2
-        }}
+        mr={{ base: 0, md: 2 }}
+        cursor="pointer"
+        onClick={() => onSelectMenuItem(defaultMenuItem)}
       >
         <Image src="/images/redditFace.svg" height="30px" />
         <Image
--- src/hooks/useDirectory.tsx
+++ src/hooks/useDirectory.tsx
@@ -1,12 +1,16 @@
+import { communityState } from "@/atoms/communitiesAtom";
 import {
   DirectoryMenuItem,
   directoryMenuState
 } from "@/atoms/directoryMenuAtom";
 import { useRouter } from "next/router";
-import { useRecoilState } from "recoil";
+import { useEffect } from "react";
+import { FaReddit } from "react-icons/fa";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 const useDirectory = () => {
   const router = useRouter();
+  const communityStateValue = useRecoilValue(communityState);
   const [directoryState, setDirectoryState] =
     useRecoilState(directoryMenuState);
 
@@ -28,6 +32,23 @@ const useDirectory = () => {
       isOpen: !directoryState.isOpen
     }));
   };
+
+  useEffect(() => {
+    const { currentCommunity } = communityStateValue;
+
+    if (currentCommunity) {
+      setDirectoryState((prev) => ({
+        ...prev,
+        selectedMenuItem: {
+          displayText: `r/${currentCommunity.id}`,
+          link: `/r/${currentCommunity.id}`,
+          imageURL: currentCommunity.imageURL,
+          icon: FaReddit,
+          iconColor: "blue.500"
+        }
+      }));
+    }
+  }, [communityStateValue.currentCommunity]);
 
   return {
     directoryState,
