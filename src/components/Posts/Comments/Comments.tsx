7b42fa70a44083f271d17f6571c2db1404b8dc1e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-14 10:13:22 +0530

    created comment component skeleton

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { Post } from "@/atoms/postAtom";
+import { User } from "@firebase/auth";
+import React, { useEffect } from "react";
 
-type CommentsProps = {};
+type CommentsProps = {
+  user: User;
+  selectedPost: Post;
+  communityId: string;
+};
 
-const Comments: React.FC<CommentsProps> = () => {
+const Comments: React.FC<CommentsProps> = ({
+  user,
+  selectedPost,
+  communityId
+}) => {
+  const onCreateComment = async (commentText: string) => {};
+  const onDeleteComment = async (comment: Comment) => {};
+  const getPostComments = async () => {};
+
+  useEffect(() => {
+    getPostComments();
+  }, []);
+
   return <div>Here Are the Comments.</div>;
 };
 export default Comments;
