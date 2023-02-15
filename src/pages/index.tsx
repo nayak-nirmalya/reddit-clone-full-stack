32f0f8ef7c2118b011b8b058aedfd694c5033e9e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-15 20:03:00 +0530

    added skeletor for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,85 @@
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { communityState } from "@/atoms/communitiesAtom";
+import { Post } from "@/atoms/postAtom";
+import CreatePostLink from "@/components/Community/CreatePostLink";
+import PageContent from "@/components/Layout/PageContent";
+import PostItem from "@/components/Posts/PostItem";
+import PostLoader from "@/components/Posts/PostLoader";
+import { auth, firestore } from "@/firebase/clientApp";
+import usePosts from "@/hooks/usePosts";
+import { Stack } from "@chakra-ui/react";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
 
 export default function Home() {
+  const [user, loadingUser] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const {
+    postStateValue,
+    setPostStateValue,
+    onDeletePost,
+    onSelectPost,
+    onVote
+  } = usePosts();
+  const communityStateValue = useRecoilValue(communityState);
+
+  const buildUserHomeFeed = () => {};
+
+  const buildNoUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        orderBy("voteStatus", "desc"),
+        limit(10)
+      );
+
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: posts as Post[]
+      }));
+    } catch (error) {
+      console.error("buildNoUserHomeFeedError", error);
+    }
+    setLoading(false);
+  };
+
+  const getUserPostVotes = () => {};
+
+  useEffect(() => {
+    if (!user && !loadingUser) buildNoUserHomeFeed();
+  }, [user, loadingUser]);
+
   return (
-    <div>Hello!</div>
-  )
+    <PageContent>
+      <>
+        <CreatePostLink />
+        {loading ? (
+          <PostLoader />
+        ) : (
+          <Stack>
+            {postStateValue.posts.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                onSelectPost={onSelectPost}
+                onDeletePost={onDeletePost}
+                onVote={onVote}
+                userIsCreator={user?.uid === post.creatorId}
+                userVoteValue={
+                  postStateValue.postVotes.find(
+                    (item) => item.postId == item.id
+                  )?.voteValue
+                }
+              />
+            ))}
+          </Stack>
+        )}
+      </>
+      <>{/* Recomendations */}</>
+    </PageContent>
+  );
 }
